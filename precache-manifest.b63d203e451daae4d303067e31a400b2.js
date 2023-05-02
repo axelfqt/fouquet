3b568b7d30a363ac3baab8e251cf6532a497b734
Axelfqt <axel.fouquet@3wa.io>
2023-05-02 09:50:19 +0200

    init

---
+++ precache-manifest.b63d203e451daae4d303067e31a400b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "327fe2dc5c98b91e4738",
+    "url": "/static/css/main.f2d04143.chunk.css"
+  },
+  {
+    "revision": "327fe2dc5c98b91e4738",
+    "url": "/static/js/main.327fe2dc.chunk.js"
+  },
+  {
+    "revision": "89ec34c9fd9cfdc9a67a",
+    "url": "/static/css/1.56eeb522.chunk.css"
+  },
+  {
+    "revision": "89ec34c9fd9cfdc9a67a",
+    "url": "/static/js/1.89ec34c9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50691c2ad2e1b27d8fa792ec59c1dad9",
+    "url": "/static/media/profil.50691c2a.jpg"
+  },
+  {
+    "revision": "c39b568590f1e983ecca0ee3d6c05dba",
+    "url": "/static/media/cv.c39b5685.pdf"
+  },
+  {
+    "revision": "4df7406874500674195d35346aa51165",
+    "url": "/index.html"
+  }
+];
